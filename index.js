cf28d5995e8af9b4df6a4e34c2c7433b3bb0f4bc
Midhun Chandra <midhun@evolvier.com>
2022-03-11 13:07:54 +0530

    index and src files added

---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2019-present, Evolvier Technologies. All rights reserved.
+ *
+ */
+
+"use strict";
+
+import React, { Component } from "react";
+
+import ReactNativeSelect from "./src";
+
+export class index extends Component {
+  render() {
+    return <ReactNativeSelect {...this.props} />;
+  }
+}
+
+export default index;
---
+++ src/index.js
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2019-present, Evolvier Technologies. All rights reserved.
+ *
+ */
+
+"use strict";
+
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+
+export class index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <View style={this.getContainerStyle()}>
+        <Text>Hello Midhun</Text>
+      </View>
+    );
+  }
+}
+
+export default index;
